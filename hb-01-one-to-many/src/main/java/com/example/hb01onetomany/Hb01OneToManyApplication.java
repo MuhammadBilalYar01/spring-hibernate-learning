75379187abb54e383c401ee5fc17cb390b5b9cd0
Muhammad Bilal <mbilalyar@gmail.com>
2022-11-29 07:56:41 +0500

    End of section 25: One to Many Directional

---
+++ hb-01-one-to-many/src/main/java/com/example/hb01onetomany/Hb01OneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hb01onetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hb01OneToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hb01OneToManyApplication.class, args);
+    }
+
+}
---
+++ hb-01-one-to-many/src/test/java/com/example/hb01onetomany/Hb01OneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hb01onetomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hb01OneToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
