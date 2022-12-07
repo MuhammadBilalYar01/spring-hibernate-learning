66d0ca782b6d1524ddf9f0abea34c796e75524d9
Muhammad Bilal <mbilalyar@gmail.com>
2022-12-07 10:15:14 +0500

    Swagger testing

---
+++ swagger-demo/src/main/java/com/example/swaggerdemo/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.example.swaggerdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer{
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("MyApp Rest APIs",
+                "APIs for MyApp.",
+                "1.0",
+                "Terms of service",
+                new Contact("test", "www.org.com", "test@emaildomain.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui/index.html");
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html");
+        registry.addRedirectViewController("/swagger", "/swagger-ui/index.html");
+    }
+}
+
